a3f78652f988703bb429503ac0265192b9bc79f8
Dmitriy Kirillov <mitkiril@gmail.com>
2022-02-21 13:42:20 +0300

    fix bugs

--- src/main/java/main/controller/DefaultController.java
+++ src/main/java/main/controller/DefaultController.java
@@ -2,12 +2,13 @@ package main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class DefaultController {
 
     @GetMapping(value = "/**/{path:[^\\.]*}")
-    public String redirectToIndex() {
+    public String redirectToIndex(@PathVariable String path) {
         return "forward:/";
     }
 }
