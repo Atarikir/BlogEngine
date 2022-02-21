1e12628d58e0894b9cc53ac4e9ade8ba10f5f202
Dmitriy Kirillov <mitkiril@gmail.com>
2022-02-21 13:31:40 +0300

    add image uploade config

---
+++ src/main/java/main/config/ImageUploadConfig.java
@@ -0,0 +1,25 @@
+package main.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class ImageUploadConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/upload/**", "/js/**", "/css/**", "/fonts/**", "/img/**",
+                        "/templates/**", "/post/upload/**", "/edit/upload/**")
+                .addResourceLocations(
+                        "file:upload/",
+                        "classpath:static/js/",
+                        "classpath:static/css/",
+                        "classpath:static/fonts/",
+                        "classpath:static/img/",
+                        "classpath:resources/templates/");
+    }
+}
